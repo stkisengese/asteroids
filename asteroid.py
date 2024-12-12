3b16d8934142473ca9db23b2e8deac5bdd2639ce
stkisengese <skisengese@outlook.com>
2024-12-12 19:00:13 +0300

    feat: add Spawning New Asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
 import pygame
 from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -18,3 +19,17 @@ class Asteroid(CircleShape):
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
         
+        new_radius = self.radius / 2
+        new_pos = self.position.copy()
+
+        # randomize the angle of the split
+        random_angle = random.uniform(20, 50)
+
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = b * 1.2
