3603302f6b47d4cbdd45612a4af5aeb4395fccb5
stkisengese <skisengese@outlook.com>
2024-12-12 18:14:51 +0300

    fix: remove velocity

--- asteroid.py
+++ asteroid.py
@@ -4,8 +4,7 @@ import pygame
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        self.velocity = pygame.Vector2(1, 1)
-
+        
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
 
